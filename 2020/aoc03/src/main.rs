5550dbe64d40488c9a3fbdf1e17f5ec927dabdd9
jesko <rattle@nullteilerfrei.de>
2021-10-27 22:48:08 +0200

    adds solution to 2020/03 part 1

---
+++ 2020/aoc03/src/main.rs
@@ -0,0 +1,82 @@
+use std::collections::HashSet;
+use std::io::Lines;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::fs::File;
+
+#[derive(Debug, Copy, Clone)]
+enum Error {
+    FileReadError,
+    FileFormatError,
+    InconsistentLineLengths
+}
+
+fn lines(filename: &str) -> Result<Lines<BufReader<File>>, Error> {
+    Ok(BufReader::new(File::open(filename).map_err(|_| Error::FileReadError)?).lines())
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+enum SquareType {
+    Open,
+    Tree
+}
+
+struct Area {
+    map: Vec<Vec<SquareType>>
+}
+
+impl Area {
+    fn new(filename: &str) -> Result<Self, Error> {
+
+        fn parse_entry(c: char) -> Result<SquareType, Error> {
+            match c {
+                '.' => Ok(SquareType::Open),
+                '#' => Ok(SquareType::Tree),
+                 _  => Err(Error::FileFormatError)
+            }
+        }
+
+        fn parse_line(line: String) -> Result<Vec<SquareType>, Error> {
+            line.chars()
+                .filter(|&c| c != '\n')
+                .map(parse_entry)
+                .collect()
+        }
+
+        let map = lines(filename)?
+            .map(|line| parse_line(line.map_err(|_|Error::FileReadError)?))
+            .collect::<Result<Vec<Vec<SquareType>>, Error>>()?;
+
+        let lengths: HashSet<usize> = map
+            .iter()
+            .map(|line| line.len())
+            .collect();
+
+        if lengths.len() != 1 {
+            Err(Error::InconsistentLineLengths)
+        } else {
+            Ok(Area{map})
+        }
+    }
+
+    fn count_trees(&self, right: usize, down: usize) -> usize {
+        let mut latitude: usize = 0;
+        let mut longitude: usize = 0;
+        let mut treecount: usize = 0;
+        while longitude < self.map.len() {
+            let contour = &self.map[longitude];
+            latitude = latitude % contour.len();
+            if contour[latitude] == SquareType::Tree {
+                treecount += 1;
+            }
+            longitude += down;
+            latitude += right;
+        }
+        treecount
+    }
+}
+
+fn main() {
+    let area = Area::new("input.txt").unwrap();
+    println!("trees: {}", area.count_trees(3, 1));
+}
