6129539f64999a0424bbb4421890a973f9e73289
jesko <rattle@nullteilerfrei.de>
2021-11-10 21:42:10 +0100

    adds part 2

--- 2020/aoc04/src/main.rs
+++ 2020/aoc04/src/main.rs
@@ -24,11 +24,71 @@ impl PassportData {
         }
     }
 
-    fn is_valid(&self) -> bool {
+    fn is_valid_pt1(&self) -> bool {
         let required_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
         required_fields
             .iter()
             .all(|&key| self.fields.contains_key(key))
+    }
+
+    #[allow(dead_code)]
+    fn to_string(&self) -> String {
+        self.fields.iter().map(|(key, value)| {
+            format!("{}: {}", key, value)
+        }).collect::<Vec<String>>().join("\n")
+    }
+
+    fn is_valid_pt2(&self) -> bool {
+        if !self.is_valid_pt1() {
+            return false;
+        } 
+
+        let is_valid_year = |key: &str, min: usize, max: usize| -> bool {
+            if let Ok(number) = self.fields[key].parse::<usize>() {
+                if number < min { return false; }
+                if number > max { return false; }
+                true
+            } else {
+                false
+            }
+        };
+
+        if !is_valid_year("byr", 1920, 2002) {
+            return false;
+        }
+        if !is_valid_year("iyr", 2010, 2020) {
+            return false;
+        }
+        if !is_valid_year("eyr", 2020, 2030) {
+            return false;
+        }
+
+        if let Some(caps) = Regex::new(r"(\d+)(cm|in)").unwrap().captures(&self.fields["hgt"]) {
+            let value: usize = caps[1].parse().unwrap();
+            let (min, max) = if &caps[2] == "cm" {(150, 193)} else {(59, 76)};
+            if value < min {
+                return false;
+            }
+            if value > max {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+        if !Regex::new(r"^#[0-9a-f]{6}$").unwrap().is_match(&self.fields["hcl"]) {
+            return false;
+        }
+
+        if !Regex::new(r"^(amb|blu|brn|gry|grn|hzl|oth)$").unwrap().is_match(&self.fields["ecl"]) {
+            return false;
+        }
+        
+        if !Regex::new(r"^\d{9}$").unwrap().is_match(&self.fields["pid"]) {
+            return false;
+        }
+
+        true
     }
 }
 
@@ -41,16 +101,21 @@ fn read_passport_data(filename: &str) -> Result<Vec<PassportData>, Error> {
         .collect())
 }
 
-fn get_valid_passport_count(filename: &str) -> Result<usize, Error> {
-    Ok((read_passport_data(filename)?)
-        .iter()
-        .filter(|data| data.is_valid())
-        .count())
-}
-
 fn main() {
-    println!(
-        "valid passport data: {}",
-        get_valid_passport_count("input.txt").unwrap()
-    );
+    
+    if let Ok(passport_data) = read_passport_data("input.txt") {
+
+        let count_pt1 = passport_data
+                .iter()
+                .filter(|data| data.is_valid_pt1())
+                .count();
+
+        let count_pt2 = passport_data
+                .iter()
+                .filter(|data| data.is_valid_pt2())
+                .count();
+    
+        println!("valid passport data for part 1: {}", count_pt1);
+        println!("valid passport data for part 2: {}", count_pt2);
+    }
 }
