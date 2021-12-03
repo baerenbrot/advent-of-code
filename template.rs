5b13080cbe38dc053e72de3d0b312070bee9dcda
jesko <rattle@nullteilerfrei.de>
2021-12-04 00:48:09 +0100

    updates template

--- template.rs
+++ template.rs
@@ -1,27 +1,45 @@
 use std::fs;
 use std::env;
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Clone)]
 enum Error {
-    FileReadError,
+    FileReadError(String),
+    MissingArgument
 }
 
 fn lines(filename: &str) -> Result<Vec<String>, Error> {
     Ok(fs::read_to_string(filename)
-        .map_err(|_| Error::FileReadError)?
+        .map_err(|_| Error::FileReadError(String::from(filename)))?
         .split('\n')
         .map(|x| String::from(x.trim()))
         .filter(|x| x.len() > 0)
         .collect::<Vec<_>>())
 }
 
-fn main() {
+fn filename() -> Result<String, Error> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        println!("Please specify input file.");
-    } else if let Ok(lines) = lines(&args[1]) {
-        // work
+        Err(Error::MissingArgument)
     } else {
-        println!("Failed to read from file: {}", args[1]);
+        Ok(args[1].clone())
+    }
+}
+
+fn main_or_error() -> Result<(), Error> {
+    let filename = filename()?;
+    let lines = lines(&filename)?;
+    
+    Ok(())
+} 
+
+fn main() {
+    match main_or_error() {
+        Ok(_) => (),
+        Err(Error::MissingArgument) => {
+            println!("Please specify input file.");
+        },
+        Err(Error::FileReadError(name)) => {
+            println!("Failed to read from file: {}", name);
+        },
     }
 }
